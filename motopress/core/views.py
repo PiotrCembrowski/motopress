df49db96522b1729184a4767d1956dd11085e03c
Piotr Cembrowski <losttape@gmail.com>
2024-10-25 16:49:57 +0200

    contact page added

--- motopress/core/views.py
+++ motopress/core/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def frontpage(request):
     return render(request, 'core/frontpage')
+
+def contact(request):
+    return render(request, 'core/contact')
--- motopress/motopress/urls.py
+++ motopress/motopress/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import frontpage
+from core.views import frontpage, contact
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', frontpage, name='frontpage'),
+    path('contact', contact, name='contact')
 ]
