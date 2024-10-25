a5157c78dfe3f2d2a2fd6013e68694365c38966e
Piotr Cembrowski <losttape@gmail.com>
2024-10-25 17:00:23 +0200

    small fixes

--- motopress/motopress/urls.py
+++ motopress/motopress/urls.py
@@ -4,6 +4,6 @@ from core.views import frontpage, contact
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('contact/', contact, name='contact'),
     path('', frontpage, name='frontpage'),
-    path('contact', contact, name='contact')
 ]
