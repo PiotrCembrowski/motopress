8ef19898529a9d0e1d80a9572908fdd4e4985907
Piotr Cembrowski <losttape@gmail.com>
2024-10-26 15:47:01 +0200

    tailwind setup

--- motopress/motopress/settings.py
+++ motopress/motopress/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,8 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core.apps.CoreConfig'
+    'core.apps.CoreConfig',
+    'tailwind',
+    'theme',
+    'django_browser_reload'
 ]
+
+TAILWIND_APP_NAME = 'theme'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -122,3 +128,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
--- motopress/motopress/urls.py
+++ motopress/motopress/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from core.views import frontpage, contact
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('contact/', contact, name='contact'),
     path('', frontpage, name='frontpage'),
+    path("__reload__/", include("django_browser_reload.urls"))
 ]
