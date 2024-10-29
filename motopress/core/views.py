79b9c676d8ce329ce0988f599faa9d83a683fbcb
Piotr Cembrowski <losttape@gmail.com>
2024-10-29 18:22:49 +0100

    adding article to views

--- motopress/core/views.py
+++ motopress/core/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
+from press.models import Article
+
 def frontpage(request):
+    articles = Article.objects.all()
     return render(request, 'core/frontpage')
 
 def contact(request):
