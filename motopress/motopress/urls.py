379f0c7a8b0d0971d43b5932d5684c6da93fcb4c
Piotr Cembrowski <losttape@gmail.com>
2024-10-29 17:56:32 +0100

    adding url static media

--- motopress/motopress/urls.py
+++ motopress/motopress/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from core.views import frontpage, contact
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('contact/', contact, name='contact'),
     path('', frontpage, name='frontpage'),
     path("__reload__/", include("django_browser_reload.urls"))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
