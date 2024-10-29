1ce5f25fa2a83263df3c56ead3f1f7683df0cc1f
Piotr Cembrowski <losttape@gmail.com>
2024-10-29 16:43:19 +0100

    pillow installed

--- motopress/press/models.py
+++ motopress/press/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    inntro = models.TextField()
+    body = models.TextField()
+    upload = models.ImageField(upload_to='uploads/')
+    created_at = models.DateTimeField(auto_now=True)
