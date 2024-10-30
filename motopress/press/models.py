beb088f7cbf9febf909e6d0238eba3cac8bdc604
Piotr Cembrowski <losttape@gmail.com>
2024-10-30 16:02:08 +0100

    adding articles to fronpage

--- motopress/press/models.py
+++ motopress/press/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
-    inntro = models.TextField()
+    intro = models.TextField()
     body = models.TextField()
     upload = models.ImageField(upload_to='uploads/')
     created_at = models.DateTimeField(auto_now=True)
