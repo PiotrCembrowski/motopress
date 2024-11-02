1fb50d00fda603a0dd21fd4f0619e98b55b5c21b
Piotr Cembrowski <losttape@gmail.com>
2024-11-02 16:27:18 +0100

    fixing typos

--- motopress/motopress/urls.py
+++ motopress/motopress/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('contact/', contact, name='contact'),
     path('post/', post, name='post'),
-    path('<slug:slug>', article, name="artciles"),
+    path('<slug:slug>', article, name="article"),
     path('', frontpage, name='frontpage'),
     path("__reload__/", include("django_browser_reload.urls"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
