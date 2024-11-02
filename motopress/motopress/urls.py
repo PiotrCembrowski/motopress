31db2fa780317296db90f122f697139a8410d989
Piotr Cembrowski <losttape@gmail.com>
2024-11-02 16:26:20 +0100

    fixing article args

--- motopress/motopress/urls.py
+++ motopress/motopress/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import frontpage, contact, post
+from core.views import frontpage, contact, post, article
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
@@ -9,7 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('contact/', contact, name='contact'),
     path('post/', post, name='post'),
-    path('<slug:slug>', views.article, name="artciles")
+    path('<slug:slug>', article, name="artciles"),
     path('', frontpage, name='frontpage'),
     path("__reload__/", include("django_browser_reload.urls"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
