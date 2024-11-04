4e07de51835d10158daf22c88173f0867a123f63
Piotr Cembrowski <losttape@gmail.com>
2024-11-04 14:29:37 +0100

    adding article model and migration

--- motopress/press/models.py
+++ motopress/press/models.py
@@ -7,6 +7,10 @@ class Article(models.Model):
     body = models.TextField()
     upload = models.ImageField(upload_to='uploads/')
     created_at = models.DateTimeField(auto_now=True)
+    banner = models.ImageField(default='image.png', blank=True)
+    
+    def __str__(self):
+        return self.title
     
 class Image(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
