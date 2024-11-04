44fc0388070c0f248624408236a9d260c487d70d
Piotr Cembrowski <losttape@gmail.com>
2024-11-04 10:40:39 +0100

    changes in slug url

--- motopress/motopress/urls.py
+++ motopress/motopress/urls.py
@@ -5,11 +5,13 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
+app_motopress = 'article'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('contact/', contact, name='contact'),
     path('post/', post, name='post'),
-    path('<slug:slug>', article, name="article"),
+    path('<slug:slug>', article, name="moto_arts"),
     path('', frontpage, name='frontpage'),
     path("__reload__/", include("django_browser_reload.urls"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
