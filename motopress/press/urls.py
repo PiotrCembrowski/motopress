173b7fec4de3d4e8d3cb688e1297cff8a40f42ba
Piotr Cembrowski <losttape@gmail.com>
2024-11-05 16:04:33 +0100

    doubling login name fixed

--- motopress/press/urls.py
+++ motopress/press/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from press.views import login
+from press.views import login_view
 
 app_name = 'press'
 
 urlpatterns = [
-    path('login/', login, name='login')
+    path('login/', login_view, name='login')
 ]
--- motopress/press/views.py
+++ motopress/press/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
 
 # Create your views here.
-def login(request):
+def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
+            login(request, form.get_user())
             return redirect("press:login")
     else:
         form = AuthenticationForm()
