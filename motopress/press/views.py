707e9bd4017111c28054292bb9b9f23d929a3550
Piotr Cembrowski <losttape@gmail.com>
2024-11-05 13:55:03 +0100

    login form added

--- motopress/press/views.py
+++ motopress/press/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 def login(request):
-    return render(request, 'press/login.html')
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            return redirect("")
+    else:
+        form = AuthenticationForm()
+    return render(request, 'press/login.html', { "form": form})
